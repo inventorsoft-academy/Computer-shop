01a27e297bc97b2a83915bbf8401b7cd9251552f
Endriu12 <github2017>
2017-08-31 00:00:31 +0300

    add validator

---
+++ src/main/java/com/perepelitsya/util/CustomValidator.java
@@ -0,0 +1,10 @@
+package com.perepelitsya.util;
+
+import java.util.HashMap;
+
+/**
+ * Created by Andriu on 8/30/2017.
+ */
+public interface CustomValidator {
+    HashMap<String, String> validate();
+}
